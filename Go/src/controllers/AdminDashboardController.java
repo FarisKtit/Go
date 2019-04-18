be0085c2f83483641cfc875bb06a6a46ac99134a
Faris Ktit <farisktit@hotmail.co.uk>
2019-04-18 21:24:14 +0100

    Updated the controllers, still incomplete

--- Go/src/controllers/AdminDashboardController.java
+++ Go/src/controllers/AdminDashboardController.java
@@ -1,5 +1,62 @@
 package controllers;
 
-public class AdminDashboardController {
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+import models.MainStorage;
+import models.User;
+
+public class AdminDashboardController {
+    @FXML
+    private ListView userListView;
+    @FXML
+    private TextField deleteUserField;
+    @FXML
+    private TextField userNameField;
+    @FXML
+    private TextField firstNameField;
+    @FXML
+    private TextField lastNameField;
+    
+    @FXML
+    public void createUser(ActionEvent event) {
+    	String userName = userNameField.getText().replaceAll("\\s+", "");
+    	String firstName = firstNameField.getText().replaceAll("\\s+", "");
+    	String lastName = lastNameField.getText().replaceAll("\\s+", "");
+    	if((userName.length() == 0) || (firstName.length() == 0) || (lastName.length() == 0)) {
+    		alertUser("Create new user", "Error", "Please make sure all fields have been filled in");
+    	} else {
+    		try {
+				MainStorage.createUser(new User(userName, firstName, lastName));
+			} catch (FileNotFoundException e) {
+	    		alertUser("Create new user", "Error", "Please try again later");
+				e.printStackTrace();
+			} catch (IOException e) {
+	    		alertUser("Create new user", "Error", "Please try again later");
+				e.printStackTrace();
+			}
+    	}
+    }
+    
+    @FXML
+    public void deleteUser(ActionEvent event) {
+    	
+    }
+    
+    private void alertUser(String title, String header, String content) {
+    	Alert alert = new Alert(AlertType.INFORMATION);
+		alert.setTitle(title);
+		alert.setHeaderText(header);
+		alert.setContentText(content);
+		alert.showAndWait();
+    }
+    
+    
+    
 }
--- Go/src/controllers/EntryDashboardController.java
+++ Go/src/controllers/EntryDashboardController.java
@@ -2,55 +2,47 @@ package controllers;
 
 import java.io.IOException;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
 import javafx.stage.Stage;
 
 public class EntryDashboardController {
 	
 	@FXML
-	private Button UserDashboardBtn;
-	public void goToUserDashboard() {
-		UserDashboardBtn.setOnAction(event -> {
-	        try {
-	        	goToView("UserDashboard", UserDashboardBtn);
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-		});
+	public void goToUserDashboard(ActionEvent event) {
+	    try {
+	        goToView("UserDashboard", event);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 	
 	@FXML
-	private Button GameDashboardBtn;
-	public void goToGameDashboard() {
-		GameDashboardBtn.setOnAction(event -> {
-	        try {
-	        	goToView("GameDashboard", GameDashboardBtn);
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-		});
+	public void goToGameDashboard(ActionEvent event) {
+	    try {
+	        goToView("GameDashboard", event);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 	
 	@FXML
-	private Button AdminDashboardBtn;
-	public void goToAdminDashboard() {
-		AdminDashboardBtn.setOnAction(event -> {
-	        try {
-	        	goToView("AdminDashboard", AdminDashboardBtn);
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-		});
+	public void goToAdminDashboard(ActionEvent event) {
+	    try {
+	        goToView("AdminEntry", event);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 	
-	public void goToView(String view, Button obj) throws IOException {
+	public void goToView(String view, ActionEvent e) throws IOException {
 		Parent userDash = FXMLLoader.load(getClass().getResource("./../Views/" + view + ".fxml"));
 		Scene s = new Scene(userDash);
-		Stage stage = (Stage) obj.getScene().getWindow();
+		Stage stage = (Stage) ((Node) e.getTarget()).getScene().getWindow();
 		stage.setScene(s);
 	}
 
