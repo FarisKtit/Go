593c7a53316b3f4c019178c85b686fc4b89436ce
nathanforester <49757908+nathanforester@users.noreply.github.com>
2019-04-23 00:38:30 +0100

    Add files via upload

---
+++ Go/src/models/ProfileImage.java
@@ -0,0 +1,12 @@
+public class ProfileImage {
+ private String URL;
+ public ProfileImage (String Input) {
+ this.URL = input;
+ }
+ public string getURL () {
+ return this.URL;
+ }
+ public void setURL (String Input) {
+ this.URL = input;
+ }
+}
