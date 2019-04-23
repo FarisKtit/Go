b45956e2685f2b16aa380efac38ac00b2ec91ba6
nathanforester <49757908+nathanforester@users.noreply.github.com>
2019-04-23 19:12:19 +0100

    Update UserDashboardController.java

--- Go/src/controllers/UserDashboardController.java
+++ Go/src/controllers/UserDashboardController.java
@@ -14,8 +14,71 @@ public class UserDashboardController extends GraphicalUserInterface {
 	public void initData(ArrayList<User> list) {
 		currentUser =  list.get(0);
 		System.out.println(currentUser.getProfile().getUserName());
+	        if (currentUser.getProfile().getProfileImage() == null) {
+	        ProfileImage pf = new ProfileImage(new File("emoticons/emoticon-1.png").toURI().toString());
+	        currentUser.getProfile().setProfileImage(pf);
+		}
+	        String url = currentUser.getProfile().getProfileImage().getURL();
+	        Image i = new Image(url);
+	        profileImage.setImage(i);
 	}
 	
+	public void selectImageOne() {
+        ProfileImage pf = new ProfileImage(new File("emoticons/emoticon-1.png").toURI().toString());
+        updateUserImage(pf);
+
+	}
+	
+	public void selectImageTwo() {
+		
+		ProfileImage pf = new ProfileImage(new File("emoticons/emoticon-2.png").toURI().toString());
+        updateUserImage(pf);
+	}
+	
+	public void selectImageThree() {
+		ProfileImage pf = new ProfileImage(new File("emoticons/emoticon-3.png").toURI().toString());
+        updateUserImage(pf);	
+	}
+	public void selectImageFour() {
+		ProfileImage pf = new ProfileImage(new File("emoticons/emoticon-4.png").toURI().toString());
+        updateUserImage(pf);	
+	}
+	public void selectImageFive() {
+		ProfileImage pf = new ProfileImage(new File("emoticons/emoticon-5.png").toURI().toString());
+        updateUserImage(pf);
+	}
+	public void selectImageSix() {
+		ProfileImage pf = new ProfileImage(new File("emoticons/emoticon-6.png").toURI().toString());
+        updateUserImage(pf);
+	}
+	
+	public void updateUserImage(ProfileImage pf) {
+		currentUser.getProfile().setProfileImage(pf);
+		String url = currentUser.getProfile().getProfileImage().getURL();
+		Image i = new Image(url);
+
+		boolean updated;
+		try {
+			updated = MainStorage.updateUser(currentUser);
+		} catch (Exception e) {
+			alertUser("profile image update", "Error", "Could not update profile image");
+		    return;
+		}
+		if (updated == true) {
+			
+			profileImage.setImage(i);
+		}
+		else {
+		    alertUser("profile image update", "Error", "Could not update profile image");
+		    return;
+		}
+		try {
+			currentUser = MainStorage.getUser(currentUser.getProfile().getUserName());
+		} catch (Exception e) {
+			alertUser("profile image update", "Error", "Could not update profile image");
+		    return;
+		}
+	}
 	@FXML
 	public void goToEntryDashboard(ActionEvent event) {
 	    try {
